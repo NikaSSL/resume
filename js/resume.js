f7d133b3a33310566c2cc65a5a7f144e9b1ae4a8
NikaSSL <nika_13@163.com>
2017-06-21 23:34:38 -0700

    完善第四部分
    完善第四部分经历内容的切换

--- js/resume.js
+++ js/resume.js
@@ -6,6 +6,15 @@ $(function(){
 			scrollTop:$($.attr(this,'href')).offset().top
 		},500);
 		return false;
-	})
-})
+	});
 
+	$('.exp-slide').click(function(){
+		if ($.attr(this,'exp') == 1) {
+			$('#exp1').css('opacity',"1");
+			$('#exp2').css('opacity',"0");
+		}else{
+			$('#exp2').css('opacity',"1");
+			$('#exp1').css('opacity',"0");
+		}
+	});
+})
