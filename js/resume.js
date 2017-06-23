0aa622e789803d34ea0df0e84fba608d910fb228
NikaSSL <nika_13@163.com>
2017-06-22 20:29:56 -0700

    新增第五部分
    新增第五部分——联系，第四部分作了小改动

--- js/resume.js
+++ js/resume.js
@@ -10,9 +10,13 @@ $(function(){
 
 	$('.exp-slide').click(function(){
 		if ($.attr(this,'exp') == 1) {
+			$("li[exp='2']").css('background-color','rgb(175,113,100)');
+			$("li[exp='1']").css('background-color','rgb(98,60,50)');
 			$('#exp1').css('opacity',"1");
 			$('#exp2').css('opacity',"0");
 		}else{
+			$("li[exp='1']").css('background-color','rgb(175,113,100)');
+			$("li[exp='2']").css('background-color','rgb(98,60,50)');			
 			$('#exp2').css('opacity',"1");
 			$('#exp1').css('opacity',"0");
 		}
