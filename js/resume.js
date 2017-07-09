44bfd277473005baaac9eaea764a7f62853c8a60
NikaSSL <nika_13@163.com>
2017-07-08 22:26:16 -0700

    响应式页面——导航
    新增响应式导航

--- js/resume.js
+++ js/resume.js
@@ -1,7 +1,7 @@
 $(function(){
 	//锚节点链接的平滑滚动
 	var $root = $('html,body');
-	$('.circle').click(function(){
+	$('.slip').click(function(){
 		$root.animate({
 			scrollTop:$($.attr(this,'href')).offset().top
 		},500);
