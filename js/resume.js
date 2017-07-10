7b3086cd4039949477a97c96fc90fd7a9affea7f
NikaSSL <nika_13@163.com>
2017-07-09 21:39:14 -0700

    鼠标滑动
    禁止了默认滑动行为，鼠标每次滑动一个页面高度

--- js/resume.js
+++ js/resume.js
@@ -21,4 +21,38 @@ $(function(){
 			$('#exp1').css('opacity',"0");
 		}
 	});
+
+	//鼠标滚动至页面节点
+	function fixedScrollListener(event){
+		event.preventDefault();//阻止默认的鼠标滚动行为
+		var part = $(document).height() < $('body').height() ? $(document).height() : $('body').height(); //页面高度
+		var direction = null;
+		var scrollTopOld = $(document).scrollTop();
+		var scrollTopNew = 0;
+		event = event || window.event;
+		direction = event.wheelDelta || event.detail
+		// if (event.wheelDelta) {// IE/Opera/Chrome
+		// 	direction = event.wheelDelta;
+		// }else{
+		// 	direction = event.detail;
+		// };
+		if (direction == 120 || direction == -3) {//向上翻滚
+			direction = false;
+		}else{//向下翻滚
+			direction = true;
+		};
+		scrollTopNew = (direction==true ? scrollTopOld+part:scrollTopOld-part);
+		// scrollTopNew = (direction==true ? scrollTopOld + part > part * 4 ? part*4: scrollTopOld + part : scrollTopOld - part < 0 ? 0 : scrollTopOld - part);
+		$root.animate({
+			scrollTop:scrollTopNew
+		},500);
+	}
+
+	var isFirefox = navigator.userAgent.indexOf("Firefox") != -1;
+    //Firefox事件：DOMMouseScroll、IE/Opera/Chrome事件：mousewheel
+    var mousewheel = isFirefox ? "DOMMouseScroll" : "mousewheel";
+	if (document.addEventListener) {
+		window.addEventListener(mousewheel,fixedScrollListener,false);
+	};
+
 })
